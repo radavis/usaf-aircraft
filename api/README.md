f1bb38ac0cc49776db5c08bcdbd5ae763229f98c
Richard Davis <rdavis.bacs@gmail.com>
2020-02-22 18:54:26 -0500

    Inits spring boot api

---
+++ api/README.md
@@ -0,0 +1,8 @@
+# usaf-aircraft-api
+
+Spring Boot Application
+
+## API Specification
+
+* `GET /aircraft`
+* `GET /aircraft/:id`
---
+++ api/src/main/java/mil/usaf/logux/aircraftapi/DemoApplication.java
@@ -0,0 +1,13 @@
+package mil.usaf.logux.aircraftapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
